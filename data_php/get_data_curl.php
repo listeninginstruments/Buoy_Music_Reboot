025b575aa8b0b7571002e4ce80f692fdc671a69c
listeninginstruments <60794509+listeninginstruments@users.noreply.github.com>
2021-07-23 11:57:57 -0500

    Error notification email in get data. removed v2 from CURL

--- data_php/get_data_curl.php
+++ data_php/get_data_curl.php
@@ -25,7 +25,8 @@ function getBuoyData($lakename, $siteid){
 	}
 	else
 	{
-	    echo '<br><br>Operation completed without any errors<br><br>';
+	    echo '<br><br>Curl operation completed without any errors<br><br>';
+	    print_r($curr_results);
 	}
 
 	curl_close($ch);
@@ -34,34 +35,67 @@ function getBuoyData($lakename, $siteid){
 	$json = json_decode($curr_results);
 	$curr_data = $json->data;
 
-	foreach ($curr_data as $key => $value) {
-	   //print "$key\n";
-		/*
-		there should only ever be one but 
-		this is going to change and contain 
-		the date / time info 
-		*/
-		$tempname = $key;
+	if($curr_data){
+		foreach ($curr_data as $key => $value) {
+		   //print "$key\n";
+			/*
+			there should only ever be one but 
+			this is going to change and contain 
+			the date / time info 
+			*/
+			$tempname = $key;
+		}
+
+		$explode_time = explode(" ", $tempname);
+		$temp_date = strtotime($explode_time[0]);
+		$filename_date = date("ymd", $temp_date) . "-" . $explode_time[1];//str_replace(":", "_", $explode_time[1]);
+
+		//print($tempname . " : " . $filename_date);
+		//print_r($curr_data);
+
+		$year = date("Y");
+		$month = date("m");
+		$day = date("d");
+
+		$newfile = dirname(__FILE__) . "/data-2020-$lakename/".$year."/".$month."/" . $day . "/" . $filename_date . ".json";
+
+		$dirname = dirname($newfile);
+		if (!is_dir($dirname))
+		{
+		    mkdir($dirname, 0755, true);
+		}
+
+		$fp = fopen($newfile , "w");
+
+		if (fwrite($fp, json_encode($curr_data)) === FALSE) {
+		    echo "Cannot write to file ($filename)";
+		    
+		} else {
+			echo "<h1>SUCCESS!</h1>Created $newfile ";
+		}
+
+		fclose($fp);
+
+
+
+
+	}	else {
+		echo "Something Seems Wrong with the Data";
+		
+		$to      = 'alex@listeninginstruments.com';
+		$subject = 'Buoy Error';
+		$message = 'There seems to be something wrong with get_data_curl.php';
+		$headers = 'From: a@listeninginstruments.com' . "\r\n" .
+		    'Reply-To: a@listeninginstruments.com' . "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
+
+		mail($to, $subject, $message, $headers);
+
+		exit($message);
+		
 	}
 
-	$explode_time = explode(" ", $tempname);
-	$temp_date = strtotime($explode_time[0]);
-	$filename_date = date("ymd", $temp_date) . "-" . $explode_time[1];//str_replace(":", "_", $explode_time[1]);
-
-	//print($tempname . " : " . $filename_date);
-	//print_r($curr_data);
-
-	$newfile = "/data-2020-$lakename/" . $filename_date . ".json";
-	$fp = fopen(dirname(__FILE__) . $newfile , "w");
-
-	if (fwrite($fp, json_encode($curr_data)) === FALSE) {
-	    echo "Cannot write to file ($filename)";
-	    
-	} else {
-		echo "<h1>SUCCESS!</h1>Created $newfile ";
-	}
-
-	fclose($fp);
+	
 
 
 }
